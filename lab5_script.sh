17d17836d640bb5b57accd575b58ea12705cc752
Louis <louisdumont4@gmail.com>
2023-12-15 09:40:34 +0100

    4

--- lab5_script.sh
+++ lab5_script.sh
@@ -61,3 +61,40 @@ sudo -u iotlab openssl genpkey -des3 -algorithm RSA -out private/root-ca.key -pa
 sudo -u iotlab openssl req -new -x509 -key private/root-ca.key -out certs/root-ca.crt -passin pass:iotlab2022 \
 -subj "/C=DE/ST=Hessen/L=Giessen/O=Global Trust/CN=Global Trust Root CA"
 
+# Vérifier le certificat
+openssl verify -verbose -CAfile certs/root-ca.crt certs/root-ca.crt
+
+# Aller dans le dossier PKI
+cd /home/iotlab/PKI
+
+# Créer un dossier pour une nouvelle autorité de certification HTBCA
+mkdir HTBCA
+
+# Aller dans le dossier HTBCA
+cd HTBCA
+
+# Créer la structure de dossiers pour le HTBCA
+mkdir certs reqs crl private
+
+# Définir les permissions pour le dossier 'private'
+chmod 700 private
+
+# Créer un fichier 'index.txt'
+touch index.txt
+
+# Générer une clé privée pour HTBCA
+openssl genpkey -des3 -algorithm RSA -out private/htb-ca.key -pass pass:iotlab2022
+
+# Générer une demande de certificat pour HTBCA
+openssl req -new -key private/htb-ca.key -out htb-ca.csr -passin pass:iotlab2022 \
+-subj "/C=DE/ST=Hessen/L=Giessen/O=Hightech-Biz/CN=Hightech-Biz CA"
+
+# Déplacer la CSR dans le dossier 'reqs' de RootCA
+mv htb-ca.csr ../RootCA/reqs
+
+# Aller dans le dossier RootCA
+cd ../RootCA
+
+# Signer le certificat HTBCA avec la clé de RootCA et les extensions spécifiées dans le fichier de configuration OpenSSL
+openssl x509 -req -extensions ca_cert -in reqs/htb-ca.csr -CA certs/root-ca.crt -CAkey private/root-ca.key \
+-CAcreateserial -out htb-ca.crt -days 365 -extfile ../pki-openssl.conf -passin pass:iotlab2022
